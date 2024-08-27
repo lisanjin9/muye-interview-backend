67cfe48f1e7786f6a67f38c6b333f0fc189d1c31
lisanjin9 <lisanjin9@163.com>
2024-08-27 20:05:29 +0800

    测试提交是否正确1

--- src/main/java/com/muye/controller/testCinfig.java
+++ src/main/java/com/muye/controller/testCinfig.java
@@ -9,6 +9,6 @@ package com.muye.controller;
  */
 public class testCinfig {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("test111");
     }
 }
