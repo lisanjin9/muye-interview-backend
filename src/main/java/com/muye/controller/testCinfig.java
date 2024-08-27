74622c4fc5bd5180602ac038cb7a60667b3b020d
lisanjin9 <lisanjin9@163.com>
2024-08-27 20:05:10 +0800

    测试提交是否正确

---
+++ src/main/java/com/muye/controller/testCinfig.java
@@ -0,0 +1,14 @@
+package com.muye.controller;
+
+/**
+ * @BelongsProject: muye-interview-backend
+ * @BelongsPackage: com.muye.controller
+ * @Author: San Jin
+ * @CreateTime: 2024-08-27 20:04
+ * @Version: 1.0
+ */
+public class testCinfig {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
